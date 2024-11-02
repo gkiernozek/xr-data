9d3970b57a643307fe972418b1a433fbbc42edd2
G.Kiernozek <gkiernozek@gmail.com>
2024-11-02 22:55:08 +0100

    Entites imported, init _Project structure

---
+++ Assets/_Project/Scripts/Graph/Bakery/CoordinatesAuthoring.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+using UnityEngine;
+
+class CoordinatesAuthoring : MonoBehaviour
+{
+    
+}
+
+class CoordinatesAuthoringBaker : Baker<CoordinatesAuthoring>
+{
+    public override void Bake(CoordinatesAuthoring authoring)
+    {
+        
+    }
+}
