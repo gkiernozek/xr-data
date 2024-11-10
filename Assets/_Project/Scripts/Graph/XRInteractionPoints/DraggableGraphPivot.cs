b3f9cd1b1476ca63e8f903a15ebaca88c2337e0e
G.Kiernozek <gkiernozek@gmail.com>
2024-11-10 14:00:23 +0100

    feat(xr-data-v2): PointMaterial props scaling together with graph pivot

--- Assets/_Project/Scripts/Graph/XRInteractionPoints/DraggableGraphPivot.cs
+++ Assets/_Project/Scripts/Graph/XRInteractionPoints/DraggableGraphPivot.cs
@@ -5,6 +5,12 @@ namespace XRData
 {
     public class DraggableGraphPivot : MonoBehaviour
     {
+	    [SerializeField] private Material graphPointMaterial;
+	    
+	    private static readonly int MaterialOriginProp = Shader.PropertyToID("_Origin");
+	    private static readonly int MaterialSpreadProp = Shader.PropertyToID("_Spread");
+
+	    
 	    private EntityManager entityManager;
 	    private Entity graphPivotTransformDataEntity;
 	    
@@ -19,6 +25,8 @@ namespace XRData
         private void Update()
         {
 	        entityManager.SetComponentData(graphPivotTransformDataEntity, new GraphPivotTransformData{position = transform.position, rotation = transform.rotation, scale = transform.localScale});
+	        graphPointMaterial.SetFloat(MaterialOriginProp, transform.position.y); 
+	        graphPointMaterial.SetFloat(MaterialSpreadProp, transform.localScale.y*0.125f/0.0825f); //0.0825 is a base draggable graph object scale, 0.125 is a base spread
         }
     }
 }
