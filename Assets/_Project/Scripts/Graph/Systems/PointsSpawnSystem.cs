8a9cebce6eede51866137d6b4bf4e15b12411443
G.Kiernozek <gkiernozek@gmail.com>
2024-11-11 18:25:46 +0100

    feat(xr-data-v2): cleanup

--- Assets/_Project/Scripts/Graph/Systems/PointsSpawnSystem.cs
+++ Assets/_Project/Scripts/Graph/Systems/PointsSpawnSystem.cs
@@ -1,4 +1,3 @@
-using System;
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
--- Assets/_Project/Scripts/Graph/XRInteractionPoints/DraggableGraphPivot.cs
+++ Assets/_Project/Scripts/Graph/XRInteractionPoints/DraggableGraphPivot.cs
@@ -5,12 +5,6 @@ namespace XRData
 {
     public class DraggableGraphPivot : MonoBehaviour
     {
-	    [SerializeField] private Material graphPointMaterial;
-	    
-	    private static readonly int MaterialOriginProp = Shader.PropertyToID("_Origin");
-	    private static readonly int MaterialSpreadProp = Shader.PropertyToID("_Spread");
-
-	    
 	    private EntityManager entityManager;
 	    private Entity graphPivotTransformDataEntity;
 	    
@@ -25,8 +19,6 @@ namespace XRData
         private void Update()
         {
 	        entityManager.SetComponentData(graphPivotTransformDataEntity, new GraphPivotTransformData{position = transform.position, rotation = transform.rotation, scale = transform.localScale});
-	        graphPointMaterial.SetFloat(MaterialOriginProp, transform.position.y); 
-	        graphPointMaterial.SetFloat(MaterialSpreadProp, transform.localScale.y*0.125f/0.0825f); //0.0825 is a base draggable graph object scale, 0.125 is a base spread
         }
     }
 }
